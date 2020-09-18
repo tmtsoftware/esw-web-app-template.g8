c4297eb07937c7f1722e6351f31a66ed668486b8
mausamyede <mausam.yede1@gmail.com>
2020-09-18 16:34:43 +0530

    Revert "#ESW-382: Mausam : Upgrade dependency versions"
    This reverts commit ad342a30

--- src/main/g8/project/Libs.scala
+++ src/main/g8/project/Libs.scala
@@ -5,9 +5,9 @@ object Libs {
 
   //testing
   val `akka-http-testkit`        = "com.typesafe.akka"                        %% "akka-http-testkit"        % "10.2.0"
-  val `akka-actor-testkit-typed` = "com.typesafe.akka"                        %% "akka-actor-testkit-typed" % "2.6.9"
-  val `akka-stream-testkit`      = "com.typesafe.akka"                        %% "akka-stream-testkit"      % "2.6.9"
+  val `akka-actor-testkit-typed` = "com.typesafe.akka"                        %% "akka-actor-testkit-typed" % "2.6.8"
+  val `akka-stream-testkit`      = "com.typesafe.akka"                        %% "akka-stream-testkit"      % "2.6.8"
   val `embedded-keycloak`        = "com.github.tmtsoftware.embedded-keycloak" %% "embedded-keycloak"        % "7fd5623"
-  val `mockito-scala`            = "org.mockito"                              %% "mockito-scala"            % "1.15.0"
-  val `scalatest`                = "org.scalatest"                            %% "scalatest"                % "3.2.2"
+  val `mockito-scala`            = "org.mockito"                              %% "mockito-scala"            % "1.14.8"
+  val `scalatest`                = "org.scalatest"                            %% "scalatest"                % "3.1.2"
 }
