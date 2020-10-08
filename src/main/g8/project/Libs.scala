4e02864d06878d0a28bc780427b9f0ac5120c97b
mausamyede <mausam.yede1@gmail.com>
2020-10-08 20:50:42 +0530

    #ESW-391: Mausam : Upgarde akka http version

--- src/main/g8/project/Libs.scala
+++ src/main/g8/project/Libs.scala
@@ -4,7 +4,7 @@ object Libs {
   val `esw-http-template-wiring` = "com.github.tmtsoftware.esw" %% "esw-http-template-wiring" % "$esw_version$"
 
   //testing
-  val `akka-http-testkit`        = "com.typesafe.akka"                        %% "akka-http-testkit"        % "10.2.0"
+  val `akka-http-testkit`        = "com.typesafe.akka"                        %% "akka-http-testkit"        % "10.2.1"
   val `akka-actor-testkit-typed` = "com.typesafe.akka"                        %% "akka-actor-testkit-typed" % "2.6.9"
   val `akka-stream-testkit`      = "com.typesafe.akka"                        %% "akka-stream-testkit"      % "2.6.9"
   val `embedded-keycloak`        = "com.github.tmtsoftware.embedded-keycloak" %% "embedded-keycloak"        % "0.2.0-M1"
