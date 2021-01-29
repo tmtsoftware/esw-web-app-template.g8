2b7a01ee823b2eef57f37c1714638f8fcd063616
mausamyede <mausam.yede1@gmail.com>
2021-01-29 21:10:08 +0530

    #ESW-384: Mausam : Update esw version to 0.1.0-SNAPSHOT for dev usage

--- src/main/g8/project/Libs.scala
+++ src/main/g8/project/Libs.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Libs {
-  val `esw-http-template-wiring` = "com.github.tmtsoftware.esw" %% "esw-http-template-wiring" % "0.2.1"
+  val `esw-http-template-wiring` = "com.github.tmtsoftware.esw" %% "esw-http-template-wiring" % "0.1.0-SNAPSHOT"
 
   //testing
   val `akka-http-testkit`        = "com.typesafe.akka"                        %% "akka-http-testkit"        % "10.2.1"
