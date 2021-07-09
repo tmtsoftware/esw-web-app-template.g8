6bd9330aa9394aeb8ab3ae6e86a7f05891af9305
Himanshu Mertia <22441818+mertia-himanshu@users.noreply.github.com>
2021-07-09 18:01:18 +0530

    #ESW-000: Himanshu : Fix build - upgrade embeded keycloak and akka version to resolve binary compatibility issue

--- src/main/g8/project/Libs.scala
+++ src/main/g8/project/Libs.scala
@@ -5,9 +5,9 @@ object Libs {
 
   //testing
   val `akka-http-testkit`        = "com.typesafe.akka"                        %% "akka-http-testkit"        % "10.2.4"
-  val `akka-actor-testkit-typed` = "com.typesafe.akka"                        %% "akka-actor-testkit-typed" % "2.6.14"
-  val `akka-stream-testkit`      = "com.typesafe.akka"                        %% "akka-stream-testkit"      % "2.6.14"
-  val `embedded-keycloak`        = "com.github.tmtsoftware.embedded-keycloak" %% "embedded-keycloak"        % "0.2.0"
+  val `akka-actor-testkit-typed` = "com.typesafe.akka"                        %% "akka-actor-testkit-typed" % "2.6.15"
+  val `akka-stream-testkit`      = "com.typesafe.akka"                        %% "akka-stream-testkit"      % "2.6.15"
+  val `embedded-keycloak`        = "com.github.tmtsoftware.embedded-keycloak" %% "embedded-keycloak"        % "0.4.0"
   val `mockito-scala`            = "org.mockito"                              %% "mockito-scala"            % "1.16.29"
   val `scalatest`                = "org.scalatest"                            %% "scalatest"                % "3.2.2"
 }
