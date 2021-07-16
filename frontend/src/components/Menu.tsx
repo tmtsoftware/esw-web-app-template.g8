bf54939b385bd6a3637fe1e0098b78f849ddb54d
Himanshu Mertia <22441818+mertia-himanshu@users.noreply.github.com>
2021-07-16 13:04:53 +0530

    #ESW-515: Himanshu : fix menu undefined warning

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -15,38 +15,23 @@ export const Logout = ({ logout }: { logout: () => void }): JSX.Element => (
   </Menu.Item>
 )
 
-const UnAuthenticatedMenuItems = () => {
-  return (
-    <>
-      <Menu.Item key='home'>
-        <Link to='/'>Home</Link>
-      </Menu.Item>
-      <Menu.Item key='greeting'>
-        <Link to='/greeting'>Greeting</Link>
-      </Menu.Item>
-    </>
-  )
-}
-const AuthenticatedMenuItems = () => {
-  return (
-    <>
-      <Menu.Item key='adminGreeting'>
-        <Link to='/adminGreeting'>Admin Greeting</Link>
-      </Menu.Item>
-    </>
-  )
-}
-
 export const MenuBar = (): JSX.Element => {
   const { auth, login, logout } = useAuth()
   const isAuthenticated = auth?.isAuthenticated() ?? false
 
   return (
     <Menu mode='horizontal'>
-      <UnAuthenticatedMenuItems />
+      <Menu.Item key='home'>
+        <Link to='/'>Home</Link>
+      </Menu.Item>
+      <Menu.Item key='greeting'>
+        <Link to='/greeting'>Greeting</Link>
+      </Menu.Item>
       {isAuthenticated ? (
         <>
-          <AuthenticatedMenuItems />
+          <Menu.Item key='adminGreeting'>
+            <Link to='/adminGreeting'>Admin Greeting</Link>
+          </Menu.Item>
           <Logout logout={logout} />
         </>
       ) : (
