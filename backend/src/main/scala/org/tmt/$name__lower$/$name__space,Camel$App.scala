42f361b9f1977af89faaf5fbde0c23afe7228c67
Himanshu Mertia <22441818+mertia-himanshu@users.noreply.github.com>
2021-08-01 21:58:58 +0530

    #ESW-196: Himanshu :  use random port for backend server if not provided in argument

--- backend/src/main/scala/org/tmt/$name__lower$/$name__space,Camel$App.scala
+++ backend/src/main/scala/org/tmt/$name__lower$/$name__space,Camel$App.scala
@@ -2,6 +2,7 @@ package org.tmt.$name;format="lower"$
 
 import caseapp.core.RemainingArgs
 import csw.location.api.models.Metadata
+import csw.network.utils.SocketUtils
 import esw.http.template.wiring.ServerApp
 import $name;format="space,Camel"$AppCommand.StartCommand
 
@@ -11,7 +12,7 @@ object $name;format="space,Camel"$App extends ServerApp[$name;format="space,Came
   override def run(command: $name;format="space,Camel"$AppCommand, remainingArgs: RemainingArgs): Unit =
     command match {
       case StartCommand(port) =>
-        val wiring = new $name;format="space,Camel"$Wiring(port)
+        val wiring = new $name;format="space,Camel"$Wiring(Some(port.getOrElse(SocketUtils.getFreePort)))
         start(wiring, Metadata.empty)
     }
 }
