2718d3e2ac67683996ca8668e132f645fa15989d
Himanshu Mertia <22441818+mertia-himanshu@users.noreply.githhub.com>
2022-01-31 13:52:13 +0530

    #ESW-575:  Himanshu | Manish : current akka http testit version

--- src/main/g8/$name__lower,word$-backend/project/Libs.scala
+++ src/main/g8/$name__lower,word$-backend/project/Libs.scala
@@ -4,7 +4,7 @@ object Libs {
   val `esw-http-template-wiring` = "com.github.tmtsoftware.esw" %% "esw-http-template-wiring" % "0.1.0-SNAPSHOT"
 
   //testing
-  val `akka-http-testkit`        = "com.typesafe.akka"                        %% "akka-http-testkit"        % "10.2.8"
+  val `akka-http-testkit`        = "com.typesafe.akka"                        %% "akka-http-testkit"        % "10.2.7"
   val `akka-actor-testkit-typed` = "com.typesafe.akka"                        %% "akka-actor-testkit-typed" % "2.6.18"
   val `akka-stream-testkit`      = "com.typesafe.akka"                        %% "akka-stream-testkit"      % "2.6.18"
   val `embedded-keycloak`        = "com.github.tmtsoftware.embedded-keycloak" %% "embedded-keycloak"        % "0.6.0"
