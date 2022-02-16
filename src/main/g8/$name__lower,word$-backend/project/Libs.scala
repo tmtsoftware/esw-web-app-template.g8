78bf55b901033dbffa72692bd9ce11a41258c33b
Shubham Chauhan <shubham25may1998@gmail.com>
2022-02-16 16:53:57 +0530

    #000: Update esw sha

--- src/main/g8/$name__lower,word$-backend/project/Libs.scala
+++ src/main/g8/$name__lower,word$-backend/project/Libs.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Libs {
-  val `esw-http-template-wiring` = "com.github.tmtsoftware.esw" %% "esw-http-template-wiring" % "0.4.0"
+  val `esw-http-template-wiring` = "com.github.tmtsoftware.esw" %% "esw-http-template-wiring" % "3b1cd4240f"
 
   //testing
   val `akka-http-testkit`        = "com.typesafe.akka"                        %% "akka-http-testkit"        % "10.2.7"
